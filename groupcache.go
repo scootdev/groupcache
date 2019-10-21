e5ecf6c4f30aef3e2c6d4c0f8db2805c1fe0ac64
Anthony Pratti <apratti@twitter.com>
2019-10-21 11:16:50 -0400

    Remove unneeded comment

--- groupcache.go
+++ groupcache.go
@@ -444,7 +444,6 @@ func (g *Group) Contain(ctx Context, key string) (bool, error) {
 	// track of whether the dest was already populated. One caller
 	// (if local) will set this; the losers will not. The common
 	// case will likely be one caller.
-	// destPopulated := false
 	ok, _, err := g.check(ctx, key)
 	if err != nil {
 		return true, err
